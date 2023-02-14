07ec127e935d1e0ba715aedbd799180397a1d8ed
GomBae <myg177@naver.com>
2023-02-14 11:11:00 +0900

    11

---
+++ SpringBasicProject_8_DI/src/main/java/com/sist/model/CModel.java
@@ -0,0 +1,7 @@
+package com.sist.model;
+
+public class CModel {
+	public void food() {
+		System.out.println("맛집관련 모든 기능을 가지고있");
+	}
+}
---
+++ SpringBasicProject_8_DI/src/main/java/com/sist/myapp/MainClass.java
@@ -0,0 +1,23 @@
+package com.sist.myapp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sist.model.AModel;
+import com.sist.model.BModel;
+import com.sist.model.CModel;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext app=new ClassPathXmlApplicationContext("app.xml");
+		AModel a=(AModel)app.getBean("a");
+		a.board();
+		BModel b=(BModel)app.getBean("b");
+		b.member();
+		CModel c=(CModel)app.getBean("c");
+		c.food();
+	}
+
+}
