23ca5b5b57490866ee95fe7b4e19f8dfa0272bb9
GomBae <myg177@naver.com>
2023-02-14 09:17:15 +0900

    11

---
+++ SpringBasicProject3_DI/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,85 @@
+package com.sist.dao;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EmpDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private String url,user,pwd;
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public EmpDAO(String driver) {
+		try{
+			Class.forName(driver);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getConnection() {
+		try {
+			conn=DriverManager.getConnection(url,user,pwd);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void disConnection() {
+		try {
+			if(ps!=null)ps.close();
+			if(conn!=null)conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public List<EmpVO> empListData(){
+		List<EmpVO> list=new ArrayList<EmpVO>();
+		try {
+			getConnection();
+			String sql="SELECT empno,ename,job,hiredate,sal FROM emp";
+			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			while(rs.next()) {
+				EmpVO vo=new EmpVO();
+				vo.setEmpno(rs.getInt(1));
+				vo.setEname(rs.getString(2));
+				vo.setJob(rs.getString(3));
+				vo.setHiredate(rs.getDate(4));
+				vo.setSal(rs.getInt(5));
+				list.add(vo);
+			}
+			rs.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disConnection();
+		}
+		return list;
+	}
+	
+	
+}
