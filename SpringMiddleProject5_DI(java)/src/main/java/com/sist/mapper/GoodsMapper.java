5e8dbd027548ab67e0ee4834fde4a6beaef6d021
GomBae <myg177@naver.com>
2023-02-15 16:07:16 +0900

    1

---
+++ SpringMiddleProject5_DI(java)/src/main/java/com/sist/mapper/GoodsMapper.java
@@ -0,0 +1,14 @@
+package com.sist.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.sist.vo.GoodsVO;
+
+
+public interface GoodsMapper {
+	@Select("SELECT no,goods_name FROM ${goods_tbl}")
+	public List<GoodsVO> goodsListData(Map map);
+}
